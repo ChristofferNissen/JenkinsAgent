49e9c30f1bd5439d6e65e5f0925f8112091cf2d9
Christoffer Nissen <christoffer.nissen@gmail.com>
2020-09-21 17:19:40 +0100

    removed sudo from script

--- docker-jenkins-agent/build.sh
+++ docker-jenkins-agent/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build ./docker-jenkins-agnt -t stifstof/jenkins-agent-docker-arm:$1 -t stifstof/jenkins-agent-docker-arm:latest
+docker build ./docker-jenkins-agnt -t stifstof/jenkins-agent-docker-arm:$1 -t stifstof/jenkins-agent-docker-arm:latest
